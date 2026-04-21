07071bd11c0037169a669f585ad401820492668c
Felix Lukowski <felix.lukowski@web.de>
2026-04-21 13:13:36 +0200

    build with kts from mvd

--- FarmSPT_EDC/FarmSPT_v1_build/launchers/controlplane/build.gradle.kts
+++ FarmSPT_EDC/FarmSPT_v1_build/launchers/controlplane/build.gradle.kts
@@ -1,43 +1,21 @@
 plugins {
     `java-library`
     id("application")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-}
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
-    // Core EDC Boot
-    runtimeOnly("org.eclipse.edc:boot:0.6.1")
-    runtimeOnly("org.eclipse.edc:configuration-filesystem:0.6.1")
-    runtimeOnly("org.eclipse.edc:runtime-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:api-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:jersey-core:0.6.1")
-    
-    // Control Plane
-    runtimeOnly("org.eclipse.edc:control-plane-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:control-plane-aggregate-services:0.6.1")
-    runtimeOnly("org.eclipse.edc:management-api:0.6.1")
-    runtimeOnly("org.eclipse.edc:management-api-configuration:0.6.1")
-    runtimeOnly("org.eclipse.edc:dsp:0.6.1")
-    
-    // Database
-    runtimeOnly("org.eclipse.edc:sql-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:datasources-sql:0.6.1")
-    runtimeOnly("com.h2database:h2:2.2.220")
-    
-    // Vault
-    runtimeOnly("org.eclipse.edc:vault-memory:0.6.1")
-    
-    // HTTP & JSON-LD
-    runtimeOnly("org.eclipse.edc:http:0.6.1")
-    runtimeOnly("org.eclipse.edc:json-ld:0.6.1")
+    implementation("org.eclipse.edc:control-plane-core:0.15.1")
+    implementation("org.eclipse.edc:management-api:0.15.1")
+    implementation("org.eclipse.edc:dsp:0.15.1")
+    implementation("org.eclipse.edc:asset-index-sql:0.15.1")
+    implementation("org.eclipse.edc:contract-definition-store-sql:0.15.1")
+    implementation("org.eclipse.edc:contract-negotiation-store-sql:0.15.1")
+    implementation("org.eclipse.edc:policy-definition-store-sql:0.15.1")
+    implementation("org.eclipse.edc:transfer-process-store-sql:0.15.1")
+    implementation("org.eclipse.edc:edr-index-sql:0.15.1")
+    implementation("org.eclipse.edc:vault-memory:0.15.1")
+    implementation("com.h2database:h2:2.2.220")
 }
 
 tasks.shadowJar {
--- FarmSPT_EDC/FarmSPT_v1_build/launchers/dataplane/build.gradle.kts
+++ FarmSPT_EDC/FarmSPT_v1_build/launchers/dataplane/build.gradle.kts
@@ -1,40 +1,14 @@
 plugins {
     `java-library`
     id("application")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-}
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
-    // Core EDC Boot
-    runtimeOnly("org.eclipse.edc:boot:0.6.1")
-    runtimeOnly("org.eclipse.edc:configuration-filesystem:0.6.1")
-    runtimeOnly("org.eclipse.edc:runtime-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:api-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:jersey-core:0.6.1")
-    
-    // Data Plane
-    runtimeOnly("org.eclipse.edc:data-plane-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:data-plane-http:0.6.1")
-    
-    // Database
-    runtimeOnly("org.eclipse.edc:sql-core:0.6.1")
-    runtimeOnly("org.eclipse.edc:datasources-sql:0.6.1")
-    runtimeOnly("com.h2database:h2:2.2.220")
-    
-    // Vault
-    runtimeOnly("org.eclipse.edc:vault-memory:0.6.1")
-    
-    // HTTP & JSON-LD
-    runtimeOnly("org.eclipse.edc:http:0.6.1")
-    runtimeOnly("org.eclipse.edc:json-ld:0.6.1")
+    implementation("org.eclipse.edc:data-plane-core:0.15.1")
+    implementation("org.eclipse.edc:data-plane-http:0.15.1")
+    implementation("org.eclipse.edc:vault-memory:0.15.1")
+    implementation("com.h2database:h2:2.2.220")
 }
 
 tasks.shadowJar {
