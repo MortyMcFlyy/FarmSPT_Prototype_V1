1e0fbb5efed72f78bd0ca42a8504d3ce5372db5f
Felix Lukowski <felix.lukowski@web.de>
2026-04-22 14:02:47 +0200

    rework of admin view for postgres and django

--- FarmSPT_Backend/API/django_api/admin.py
+++ FarmSPT_Backend/API/django_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FieldBoundary, ABTrace
 
-# Register your models here.
+@admin.register(FieldBoundary)
+class FieldBoundaryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'area_hectares', 'created_at']
+
+@admin.register(ABTrace)
+class ABTraceAdmin(admin.ModelAdmin):
+    list_display = ['field', 'distance_km', 'created_at']
